8895367ceb41ede444f8fac47f3c3dd2776d2d53
Keila Eneas <keilaeneas@gmail.com>
2019-02-05 18:03:38 -0200

    etapa 3

--- principal.js
+++ principal.js
@@ -1,5 +1,42 @@
-console.log("Fui carregado de um arquivo externo");
-
-document.querySelector('.titulo');
 var titulo = document.querySelector('.titulo');
 titulo.textContent = 'Aparecida Nutricionista';
+
+var pacientes = document.querySelectorAll('.paciente');
+
+for(var i=0; i<pacientes.length; i++){
+    var paciente = pacientes[i];
+
+    var tdPeso = paciente.querySelector('.info-peso');
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector('.info-altura');
+    var altura = tdAltura.textContent;
+
+    var tdImc = paciente.querySelector('.info-imc');
+
+    var alturaEhValda = true;
+    var pesoEhValido = true;
+
+    if(peso <= 0 || peso >= 1000){
+        console.log('Peso inválido!');
+        tdPeso.textContent = "Peso inválido!";
+        pesoEhValido = false;
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if(altura <= 0 || altura >= 3){
+        console.log('Altura inválida!');
+        tdAltura.textContent = "Altura inválida!";
+        alturaEhValda = false;
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if(alturaEhValda && pesoEhValido){
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }else{
+        tdImc.textContent = "Altura e/ou peso inválidos!"
+    }
+}
+
+
